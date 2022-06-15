78584dc979a8f0db46006473061c5943f5f6ab2b
Wackyator <pratapsinghtarun9@gmail.com>
2022-06-15 14:12:03 +0530

    finished bulk inserts

--- bulk_queries.py
+++ bulk_queries.py
@@ -1,15 +1,43 @@
 from config import DBCreds
 
+from uuid import uuid4
+
 import mysql.connector
 
-db = mysql.connector.connect(
-  host = DBCreds.host,
-  port = DBCreds.port,
-  user = DBCreds.username,
-  passwd = DBCreds.passwd,
-  database = DBCreds.name,
-)
+def get_db(creds):
+  db = mysql.connector.connect(
+    host = creds.host,
+    port = creds.port,
+    user = creds.username,
+    passwd = creds.passwd,
+    database = creds.name,
+  )
+  return db
 
-cur = db.cursor()
-cur.execute("SELECT CURTIME()")
-print(cur.fetchone())
+def create_table(cursor):
+  """function to create the table for bulk inserts"""
+  query = "CREATE TABLE User (id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL)"
+
+  cursor.execute(query)
+
+def bulk_insert(cursor):
+  """function doing performing the bulk inserts"""
+  query = "INSERT INTO User (name) VALUES (%s)"
+  data = [(f"{uuid4()}",) for i in range(2000)]
+
+  cursor.executemany(query, data)
+
+def main():
+  db = get_db(DBCreds)
+  cur = db.cursor()
+  create_table(cur)
+  bulk_insert(cur)
+  db.commit()
+  cur.execute("SELECT * FROM User")
+  cur.fetchall()
+  print(cur.rowcount)
+  # cur.execute("DROP TABLE User")
+  # db.commit()
+  
+if __name__ == "__main__":
+  main()
